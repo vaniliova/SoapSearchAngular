fba95d87c48111a2603e09adea9433c26bfbbcf6
Dagmara Lewandowska <vanila.dl@gmail.com>
2017-06-16 19:26:53 +0200

    new factory, separate json from controller

--- scripts/firstngcontroller.js
+++ scripts/firstngcontroller.js
@@ -2,6 +2,13 @@ angular
   .module('FirstNgApp')
   .controller('firstNgController', function($scope, firstNgFactory) {
 
-    $scope.first = firstNgFactory.getFirstNg();
+	  	$scope.recipes;
 
-  });
+	  	firstNgFactory.getFirstNg.then(function successCallback(data) {
+	  		$scope.recipes = data.data;
+	  	}, function errorCallback(error) {
+	  		console.log(error.json);
+	  	
+
+   });
+
--- scripts/firstngfactory.js
+++ scripts/firstngfactory.js
@@ -1,32 +1,14 @@
 angular
-  .module('FirstNgApp')
-  .factory('firstNgFactory', function() {
+	.module('FirstNgApp')
+	.factory('firstNgFactory', function($http) {
 
-  	var firstNgData = [
-	    {
-	       "type": "Condo",
-	       "price": 220000,
-	       "address": "213 Grove Street",
-	       "description": "Excellente place, really nice view!"
-	    },
-	    {
-	    	"type": "House",
-	    	"price": 410500,
-	    	"address": "245 Winding Place",
-	    	"description": "Beautyful home with lots of space for big family!"
-	    },
-	    {
-	    	"type": "Duplex",
-	    	"price": 390500,
-	    	"address": "657 River Lane",
-	    	"description": "Great neibourhood and lot's of green space!"
-	    }
-	    ];
-    
-    function getFirstNg() {
-    	return(firstNgData);
-    }
-      return {
-      	getFirstNg: getFirstNg
-      }
-  });
+
+	  	function getFirstNg() {
+	  		return $http.get('data/data.json');
+	  	}
+
+	  	return {
+	  		getFirstNg: getFirstNg
+	  	}
+
+	});
